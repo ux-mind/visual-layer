4f1d9a93136487ec69f933bc294daa36336130b6
Dima Frid <dima.frid@gmail.com>
2023-05-17 10:18:44 +0300

    publicRoutes.js - added link to techcrunch article to 'In Press'

--- src/js/data/publicRoutes.js
+++ src/js/data/publicRoutes.js
@@ -6,7 +6,7 @@ const PUBLIC_ROUTES = {
 	privacy_policy: '/privacy-policy',
 	terms_of_use: '/terms-of-use',
 	request_a_demo: '/request-a-demo',
-	in_press: ''
+	in_press: 'https://techcrunch.com/2023/05/16/visual-layer-helps-enterprise-manage-the-massive-visual-data-sets-they-need-to-build-ai-models-raises-7m/'
 };
 
 export default PUBLIC_ROUTES;
